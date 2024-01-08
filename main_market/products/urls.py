d8996560140814e304ba4c273018753ed0de8863
pramitsingh0 <pramitsingh0@gmail.com>
2024-01-08 19:41:41 +0530

    added product saving logic

--- main_market/products/urls.py
+++ main_market/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import create_product_view
+from .views import create_product_view, save_product
 
 app_name = "products"
 urlpatterns = [
     path("create/", create_product_view, name="create_view"),
+    path("save-product/", save_product, name="save_product"),
 ]
--- main_market/products/views.py
+++ main_market/products/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from .models import Product
 
 
 # Create your views here.
 #
 #
+
 def create_product_view(request):
     return render(request, "marketplace/products/create.html")
+
+
+def save_product(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "Method Error! Only POST request allowed"})
+    prod_name = request.POST.get("name")
+    prod_handler = request.POST.get("handler")
+    price = request.POST.get("price")
+    product = Product(name=prod_name, handle=prod_handler, price=float(price))
+    try:
+        product.save()
+        return HttpResponse(b"Product Saved", status=200)
+        # return HttpResponseRedirect(reverse("products:product_listing"))
+    except Exception as e:
+        print(e)
+        return JsonResponse({"error": "Error Saving product"}, status=500)
